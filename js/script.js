c16ce55e2442c4e5173ae12eacd9337533c0af71
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-03-25 07:58:26 +0100

    Change javascript code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
 }
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+printMessage('Mój ruch: ' + computerMove);
