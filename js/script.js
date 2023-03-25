9e5827c09337dab1eb0dd8ec46175a43bbbbdd33
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-03-25 08:08:29 +0100

    Add script for player

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ if (randomNumber == '1') {
   computerMove = 'nożyce';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
