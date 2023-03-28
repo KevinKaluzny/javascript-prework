174e6ada11e0a7a541650ca538afa2234059cd3a
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-03-28 19:40:37 +0200

    Make variable computerMove const

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var computerMove, playerInput, playerMove, randomNumber;
+var playerInput, playerMove, randomNumber;
 var playerWins = 0;
 var computerWins = 0;
 
@@ -67,7 +67,7 @@ function buttonClicked(argButtonName) {
 	console.log('ruch gracza to: ' + playerMove);
 	randomNumber = Math.floor(Math.random() * 3 + 1);
 	console.log('wylosowana liczba to: ' + randomNumber);
-	computerMove = getMoveName(randomNumber);
+	const computerMove = getMoveName(randomNumber);
 	console.log('ruch komputera to: ' + computerMove);
 	displayResult(playerMove, computerMove);
 }
